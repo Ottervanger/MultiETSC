105e47bf3f0cadb92c6ed89f7fc447738797fd7b
Gilles Ottervanger <gilles.ottervanger@asgnederland.nl>
2020-05-10 23:23:16 +0200

    Fix style

--- ECTS/cpp/find.cpp
+++ ECTS/cpp/find.cpp
@@ -1,22 +1,11 @@
 #include "find.h"
 
-int find(double a[], double target, int len)
-{   
-	
-	int count=0;
-	for  (int i=0;i<len;i++)
-	{
-	  if (a[i]==target)
-	  {
-	    count++;
-
-	  }
-	
-	}
-
-   return count;
-
+int find(double a[], double target, int len) {
+    int count = 0;
+    for  (int i = 0; i < len; i++) {
+        if (a[i] == target) {
+            count++;
+        }
+    }
+    return count;
 }
-
-
-
