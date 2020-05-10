cd3d85e3ac14a1f4b0708577553d397b948d580d
Gilles Ottervanger <gillesottervanger@hotmail.com>
2020-05-10 23:55:12 +0200

    Refactor

--- ECTS/cpp/Euclidean.cpp
+++ ECTS/cpp/Euclidean.cpp
@@ -1,16 +1,12 @@
-#include "math.h"
 #include "Euclidean.h"
 
-
-
-double Euclidean(double * a, double * b, int length)
-{
-  double ret = 0;
-  for (int i=0; i<length;i++)
-  {
-    double dist = a[i]-b[i];
-    ret += dist * dist;
-  }
-  return ret > 0 ? sqrt(ret) : 0;
-  //return ret > 0 ? ret : 0;
+double Euclidean(double * a, double * b, int length) {
+    double ret = 0;
+    for (int i = 0; i < length; i++) {
+        double dist = a[i] - b[i];
+        ret += dist * dist;
+    }
+    // since we only use the distance to order 
+    // we can leave out taking the root
+    return ret;
 }
