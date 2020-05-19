76a9024deeadd95601d36ee908c8b7345f8506af
Gilles Ottervanger <gillesottervanger@hotmail.com>
2020-05-19 21:58:54 +0200

    Use std::strings in util

--- ECTS/cpp/util.h
+++ ECTS/cpp/util.h
@@ -2,22 +2,23 @@
 #define ECTS_UTIL_H_
 
 #include <vector>
+#include <string>
 
 namespace util {
 
 // load data in UCR format to the 2d vector 'data' with corresponding 'labels'
-bool readUCRData(const char * file,
+bool readUCRData(const std::string &file,
                  std::vector<std::vector<double> > &data,
                  std::vector<int> &labels);
 
 // load whitespace separated data from file to 2d vector
 template<typename T>
-bool readDMatrix(const char * file,
+bool readDMatrix(const std::string &file,
                  std::vector<std::vector<T> > &data);
 
 // save tab separated data from file to 2d vector
 template<typename T>
-bool saveMatrix(const char * file,
+bool saveMatrix(const std::string &file,
                 const std::vector<std::vector<T> > &data);
 
 } // namespace util
