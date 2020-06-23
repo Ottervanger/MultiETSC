c22cf832fbce56104da3e452b5f6db70297780d5
G.B. Ottervanger <g.b.ottervanger@umail.leidenuniv.nl>
2020-06-23 19:25:33 +0200

    Refine plotter; Add results to project

---
+++ experiments/plot.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+import numpy as np
+import os
+import sys
+import re
+import glob
+
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+def usage():
+    print('Please provide glob for input files:\n  {} ECG200*')
+    sys.exit()
+
+def colors(opaque=False):
+    while True:
+        for c in ['#1f77b4', # blue
+                  '#ff7f0e', # orange
+                  '#2ca02c', # green
+                  '#d62728', # red
+                  '#9467bd', # purple
+                  '#8c564b', # brown
+                  '#e377c2', # pink
+                  '#7f7f7f', # grey
+                  '#bcbd22', # olive
+                  '#17becf'  # cyan
+                 ]:
+            yield c if not opaque else c + '99'
+
+class Pareto:
+    def  __init__(self, Y, metadata, label):
+        inp = np.ones(len(Y), dtype="bool")
+        for i in range(len(Y)):
+            if inp[i]:
+                inp = inp & np.any(Y < Y[i],1)
+                inp[i] = True
+        self.P = Y[inp]
+        self.metadata = metadata[inp]
+        self.size = len(self.P)
+        self.label = label
+
+    def plot(self, ax, c, textoffset):
+        ax.plot(self.P[:,0],self.P[:,1], '-o', c=c, label=self.label, linewidth=1, markersize=3)
+        # adding labels
+        labels = [re.sub('^-algorithm ([^/]*)[^\s]* (.*)$', '\\1 \\2', i.replace("'", '')) for i in self.metadata]
+        for x, y, label in zip(self.P[:,0], self.P[:,1], labels):
+            # label
+            txt = ax.annotate(label, (x, y), textcoords="axes fraction", xytext=textoffset['xy'],
+                        ha='left', va='center', fontsize=8, clip_on=False,
+                        bbox=dict(boxstyle="round", fc=(1,1,1,.8), ec='.8'),
+                        arrowprops=dict(arrowstyle='-|>', linestyle='--', color=c+'aa',
+                        connectionstyle="angle,angleA=180,angleB=75", relpos=(0., .5),
+                        shrinkA=10, shrinkB=10))
+            textoffset['xy'][1] -= 0.06
+
+    def hmean(self):
+        return np.min(1-self.P.shape[1]/np.sum((1-self.P)**-1,1))
+
+    def hypervolume(self):
+        '''
+        compute dominated hypervolume given pareto set Y
+        pre: Y consists of two dimensional non-dominated
+             points sorted by the first objective.
+        '''
+        ref = np.array([1.,1.])
+        s = 0
+        for y in self.P:
+            s += np.prod(ref-y)
+            ref[1] = y[1]
+        return s
+
+    def M3(self):
+        return np.linalg.norm(self.P[0]-self.P[-1])
+
+    def delta(self):
+        di = np.zeros(self.size-1)
+        for i in range(len(di)):
+            di[i] = np.linalg.norm(self.P[i]-self.P[i+1])
+        df = np.linalg.norm(self.P[0]-np.array([0,1]))
+        dl = np.linalg.norm(self.P[-1]-np.array([1,0]))
+        return (df+dl+np.sum(np.abs(di-np.mean(di))))/(df+dl+np.sum(di))
+
+def getData(csvPath):
+    # read data from csv
+    pat = re.compile('\[(.*), (.*)\], 0, (.*)')
+    with open(csvPath) as f:
+        data = np.array([re.findall(pat, s)[0] for s in f.readlines()])
+    # sort by the first objective
+    data = data[np.argsort(data[:,0])]
+    Y = np.array(data[:,:2], dtype=float)
+    metadata = data[:,2]
+    return Y, metadata
+
+def main():
+    color = colors()
+    ruler = 1.02
+    textoffset = dict(xy=[ruler,.98])
+    if (len(sys.argv) < 2):
+        usage()
+    files = glob.glob('output/results/'+sys.argv[1])
+    if not len(files):
+        usage()
+    print('Files found:')
+    for f in files:
+        print('  '+f)
+    from matplotlib import pyplot as plt
+    fig, ax = plt.subplots(figsize=(8,4.8))
+    for f in files:
+        try:
+            Y, metadata = getData(f)
+            label = f.split('/')[-1].split('.')[0]
+            pareto = Pareto(Y, metadata, label=label)
+            pareto.plot(ax, c=next(color), textoffset=textoffset)
+            textoffset['xy'][1] -= 0.05
+            sf = '|{:>12s}: {:8.4f}'
+            sd = '|{:>12s}: {:3d}'
+            print('| {}'.format(label))
+            print(sd.format('size', pareto.size))
+            print(sf.format('delta', pareto.delta()))
+            print(sf.format('M3', pareto.M3()))
+            print(sf.format('min hmean', pareto.hmean()))
+            print(sf.format('hypervolume', pareto.hypervolume()))
+            print()
+            
+        except FileNotFoundError:
+            continue
+    dataname = files[0].split('/')[-1].split('-')[0]
+    seed = re.sub('.*-([0-9]*)\\..*', '\\1', files[0])
+    ax.set_xlim([0, 1])
+    ax.set_ylim([0, 1])
+    ax.grid()
+    ax.legend(loc=(ruler,0))
+    plt.title("Earliness-Accuracy tradeoff\n{}".format(dataname))
+    plt.xlabel("Earliness")
+    plt.ylabel("Error rate")
+
+    fig.tight_layout()
+    plt.savefig('output/plots/{}-{}.pdf'.format(dataname, seed))
+
+if __name__ == '__main__':
+    main()
