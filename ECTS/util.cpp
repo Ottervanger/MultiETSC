a1becc8352d1ad2052783549dcb8a2089a6e9390
G.B. Ottervanger <g.b.ottervanger@umail.leidenuniv.nl>
2020-08-22 15:04:46 +0200

    Simplification of ects; Update tests

--- ECTS/util.cpp
+++ ECTS/util.cpp
@@ -34,45 +34,4 @@ bool readUCRData(const std::string &f,
     return true;
 }
 
-
-template bool readDMatrix(const std::string &f, std::vector<std::vector<int> > &data);
-template bool readDMatrix(const std::string &f, std::vector<std::vector<double> > &data);
-
-template<typename T>
-bool readDMatrix(const std::string &f, std::vector<std::vector<T> > &data) {
-    std::ifstream ifs(f);
-    if (ifs.fail()) {
-        std::cerr << "File \'" << f << "\' could not be opened for reading" << std::endl;
-        return false;
-    }
-    std::string line;
-    std::vector<T> row;
-    while (std::getline(ifs, line)) {
-        std::stringstream ss(line);
-        T v;
-        while (ss >> v) row.push_back(v);
-        data.push_back(row);
-        row.clear();
-    }
-    return true;
-}
-
-template bool saveMatrix(const std::string &f, const std::vector<std::vector<int> > &data);
-template bool saveMatrix(const std::string &f, const std::vector<std::vector<double> > &data);
-
-template<typename T>
-bool saveMatrix(const std::string &f, const std::vector<std::vector<T> > &data) {
-    std::ofstream ofs(f);
-    if (ofs.fail()) {
-        std::cerr << "File \'" << f << "\' could not be opened for writing" << std::endl;
-        return false;
-    }
-    for (auto r : data) {
-        for (T v : r)
-            ofs << v << "\t";
-        ofs << std::endl;
-    }
-    return true;
-}
-
 } // namespace util
--- ECTS/util.h
+++ ECTS/util.h
@@ -11,16 +11,6 @@ bool readUCRData(const std::string &file,
                  std::vector<std::vector<double> > &data,
                  std::vector<int> &labels);
 
-// load whitespace separated data from file to 2d vector
-template<typename T>
-bool readDMatrix(const std::string &file,
-                 std::vector<std::vector<T> > &data);
-
-// save tab separated data from file to 2d vector
-template<typename T>
-bool saveMatrix(const std::string &file,
-                const std::vector<std::vector<T> > &data);
-
 } // namespace util
 
 #endif  // ECTS_UTIL_H_
