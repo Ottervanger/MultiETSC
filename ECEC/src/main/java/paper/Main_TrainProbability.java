e8555cce89a688f8acd761e134c25aa906a22b94
G.B. Ottervanger <g.b.ottervanger@umail.leidenuniv.nl>
2020-10-07 19:18:01 +0200

    Remove TimeSeriesSet dependency; Using sfa TimeSeries arrays instead

--- ECEC/src/main/java/paper/Main_TrainProbability.java
+++ ECEC/src/main/java/paper/Main_TrainProbability.java
@@ -2,35 +2,30 @@ package paper;
 
 import java.io.File;
 
-import DataStructures.DataLoader;
-import DataStructures.TimeSeriesInstance;
-import DataStructures.TimeSeriesSet;
+
 import Classifiers.sfa.classification.ParallelFor;
+import Classifiers.sfa.timeseries.TimeSeries;
+import Classifiers.sfa.timeseries.TimeSeriesLoader;
+
 import DataStructures.EarlyClassifierResult;
-import DataStructures.ProbabilityInformation;
 
 public class Main_TrainProbability {
     
     public static void main(String[] args){
         long startTime = System.currentTimeMillis();
         String dir = "/scratch/ottervanger/UCR/";
-        String result_dir = "./out/probability/";
-
         String dataset = "CBF";
-        String train_file = dir + dataset + File.separator + dataset + "_TRAIN.tsv";
-        String test_file = dir + dataset + File.separator + dataset + "_TEST.tsv";
+
+        String trainFile = dir + dataset + File.separator + dataset + "_TRAIN.tsv";
+        String testFile = dir + dataset + File.separator + dataset + "_TEST.tsv";
         
-        String result_dir2 = result_dir + dataset;
-        File file = new File(result_dir2);  
-        if (!file.exists())
-            file.mkdirs();
-        
-        TimeSeriesSet train_data = DataLoader.loadSourceData(train_file);
-        TimeSeriesSet test_data = DataLoader.loadSourceData(test_file);
+        TimeSeries.APPLY_Z_NORM = false;
+        TimeSeries[] trainSamples = TimeSeriesLoader.loadDataset(trainFile);
+        TimeSeries[] testSamples = TimeSeriesLoader.loadDataset(testFile);
         
         ProbabilityTrainer_General_Memory classifer = new ProbabilityTrainer_General_Memory();
         EarlyClassifier_ProbThreshold classifier2 = new EarlyClassifier_ProbThreshold();
-        EarlyClassifierResult result = classifier2.predict(classifer.process(train_data, test_data, result_dir2));
+        EarlyClassifierResult result = classifier2.predict(classifer.process(trainSamples, testSamples));
 
         double elapsedTime = (System.currentTimeMillis() - startTime) / 1000.;
         System.out.printf(
